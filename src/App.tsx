59264a4b8707ee41efa6fce261a36a6209dd4033
joelcoelho29 <joelfeco4@gmail.com>
2023-10-11 00:03:22 -0300

    feat: implemented architecture purpose to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from '@nextui-org/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoutes } from '@/routes';
 
 function App() {
   return (
       <NextUIProvider>
-          <div></div>
+        <BrowserRouter>
+          <Routes>
+            <Route path={AppRoutes.home} element={<Home />} />
+          </Routes>
+        </BrowserRouter>
       </NextUIProvider>
   );
+}
+
+// TODO: remove Home after defining routes
+function Home() {
+  return <div>Home</div>
 }
 
 export default App;
---
+++ src/presentation/hooks/use-fetch.ts
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+export enum Status {
+  idle,
+  loading,
+  error,
+  success,
+}
+export interface State<T> {
+  data?: T
+  error?: Error
+  status: Status
+}
+export function useFetch<T = unknown>(
+  fn: () => Promise<T>,
+): { fetch: () => Promise<void>; state: State<T>; } {
+  const initialState: State<T> = {
+    error: undefined,
+    data: undefined,
+    status: Status.idle
+  }
+
+  const [state, setState] = useState(initialState)
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  async function fetch(): Promise<void> {
+    setState({
+      status: Status.loading,
+    })
+
+    try {
+      const result = await fn()
+
+      setState({
+        data: result, status: Status.success,
+      })
+    } catch (e) {
+      setState({
+        status: Status.error,
+        error: e as Error
+      })
+    }
+  }
+
+  return { fetch, state }
+}
