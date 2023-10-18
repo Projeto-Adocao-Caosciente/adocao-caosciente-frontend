9079100ba99da153d39cc0472e9139206c49e80a
Ryan S <ryansouza@usp.br>
2023-10-18 20:56:55 -0300

    feat: definindo breakpoints de responsividade no tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      'xs': "320px",
+      'sm': '480px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '[2xl]': '1536px',
+    },
     extend: {},
   },
   darkMode: 'class',
