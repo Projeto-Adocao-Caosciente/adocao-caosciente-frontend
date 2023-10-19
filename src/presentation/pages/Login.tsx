61ee77baca5d04b2e7ee3b17c4f66a23d3e8ecac
Vasini25 <flavippolini@yahoo.com.br>
2023-10-19 11:55:11 -0300

    CT-24: Adicionado texto de Login no cabeçalho

--- src/presentation/pages/Login.tsx
+++ src/presentation/pages/Login.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export default function Login() {
   return (
-    
-    <div>Login</div>
+    <>
+      <Navbar />
+        <main className="xs:px-5 md:px-20 lg:px-40">
+          {/* TODO: adicionar logo */}
+          <h1 className="text-center xs:text-2xl md:text-3xl font-bold py-16">
+            Login
+          </h1>
+        </main>
+    </>
   )
 }
