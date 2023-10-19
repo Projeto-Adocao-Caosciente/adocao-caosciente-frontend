c1c1c1b2d1c4eee8df6626bd89a46b7735a5eb6e
Vasini25 <flavippolini@yahoo.com.br>
2023-10-19 13:46:13 -0300

    CT-24: adicionados seção de input para CNPJ e senha

--- src/presentation/pages/Login.tsx
+++ src/presentation/pages/Login.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { Divider, Input } from '@nextui-org/react'
 
 export default function Login() {
   return (
     <>
       <Navbar />
-        <main className="xs:px-5 md:px-20 lg:px-40">
+        <main className="xs:mx-5 md:mx-20 lg:mx-72">
           {/* TODO: adicionar logo */}
-          <h1 className="text-center xs:text-2xl md:text-3xl font-bold py-16">
+          <header className="text-center text-4xl font-bold py-16">
             Login
-          </h1>
+          </header>
+          <h2 className='text-center text-xl font-light'>
+            Anuncie seus pets para adoção e gerencie seus formulários
+          </h2>
+          {/* TODO: Acertar tamanho do divider */}
+          <Divider className="my-6" />
+          {/* TODO: Acertar tamanho lateral da section */}
+          <section className="flex justify-center gap-4 flex-col items-center">
+            <Input
+              placeholder="CNPJ"
+              variant="bordered"
+              size="lg"
+            />
+            <Input
+              placeholder="Senha"
+              variant="bordered"
+              size="lg"
+            />
+          </section>
         </main>
     </>
   )
