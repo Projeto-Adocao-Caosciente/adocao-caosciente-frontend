c0aa76ea86fcc761c40b5a8d7833405f72c1be92
joelcoelho29 <joelfeco4@gmail.com>
2023-10-22 16:39:05 -0300

    style: finished css styling in pet page

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import { NextUIProvider } from '@nextui-org/react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AppRoutes } from './routes/app-routes'
 import Home from './presentation/pages/Home'
-import PetRegister from './presentation/pages/PetRegister'
+import PetRegister from './presentation/pages/PetPage'
 
 function App() {
-  return (
-    <NextUIProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path={AppRoutes.home} element={<Home />} />
-          <Route path={AppRoutes.petRegister} element={<PetRegister />} />
-        </Routes>
-      </BrowserRouter>
-    </NextUIProvider>
-  )
+    return (
+        <NextUIProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={AppRoutes.home} element={<Home />} />
+                    <Route
+                        path={AppRoutes.petRegister}
+                        element={<PetRegister />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </NextUIProvider>
+    )
 }
 
 export default App
