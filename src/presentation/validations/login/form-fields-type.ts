ee62f5e28debc85daa1707a80b6c3510913a2f97
joel_coelho <61102108+joelcoelho29@users.noreply.github.com>
2023-10-25 00:45:08 -0300

    Update src/presentation/validations/login/form-fields-type.ts
    Co-authored-by: Ryan Souza <76923948+Ryrden@users.noreply.github.com>

--- src/presentation/validations/login/form-fields-type.ts
+++ src/presentation/validations/login/form-fields-type.ts
@@ -21,6 +21,7 @@ export class LoginFieldsValidationWrapperImpl
 
     patterns: FieldPatternMap<LoginFormFields> = {
         user: {
+            // Regex for CNPJ - 00.000.000/0000-00
             matcher: /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/,
             apply: (value) =>
                 value
