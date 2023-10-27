fc324e9e984e4c7f49ce93fc7e6cbccad84d32ec
joelcoelho29 <joelfeco4@gmail.com>
2023-10-26 23:16:32 -0300

    adjusted useFetch promise arguments

--- src/presentation/hooks/use-fetch.ts
+++ src/presentation/hooks/use-fetch.ts
@@ -1,50 +1,50 @@
 import { useState } from 'react'
 
 export enum Status {
-  idle,
-  loading,
-  error,
-  success,
+    idle,
+    loading,
+    error,
+    success,
 }
 export interface State<T> {
-  data?: T
-  error?: Error
-  status: Status
+    data?: T
+    error?: Error
+    status: Status
 }
 export function useFetch<T = unknown>(
-  fn: () => Promise<T>
+    fn: (...args: any[]) => Promise<T>
 ): {
-  fetch: () => Promise<void>
-  state: State<T>
+    fetch: (...args: any[]) => Promise<void>
+    state: State<T>
 } {
-  const initialState: State<T> = {
-    error: undefined,
-    data: undefined,
-    status: Status.idle,
-  }
-
-  const [state, setState] = useState(initialState)
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  async function fetch(): Promise<void> {
-    setState({
-      status: Status.loading,
-    })
-
-    try {
-      const result = await fn()
-
-      setState({
-        data: result,
-        status: Status.success,
-      })
-    } catch (e) {
-      setState({
-        status: Status.error,
-        error: e as Error,
-      })
+    const initialState: State<T> = {
+        error: undefined,
+        data: undefined,
+        status: Status.idle,
     }
-  }
 
-  return { fetch, state }
+    const [state, setState] = useState(initialState)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    async function fetch(...args: any[]): Promise<void> {
+        setState({
+            status: Status.loading,
+        })
+
+        try {
+            const result = await fn(...args)
+
+            setState({
+                data: result,
+                status: Status.success,
+            })
+        } catch (e) {
+            setState({
+                status: Status.error,
+                error: e as Error,
+            })
+        }
+    }
+
+    return { fetch, state }
 }
