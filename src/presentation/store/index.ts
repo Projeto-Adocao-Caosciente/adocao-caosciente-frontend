975e08bd57f660053cf7e708642974c8351a1725
joelcoelho29 <joelfeco4@gmail.com>
2023-11-13 23:16:38 -0300

    refactor: refactored naming on reducers and deleted unused animalsReducer

--- src/presentation/store/index.ts
+++ src/presentation/store/index.ts
@@ -1,11 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../reducer/userReducer';
-import animalsReducer from '../reducer/animalsReducer';
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from '../reducer/user-reducer'
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        animals: animalsReducer,
-    }
+    },
 })
-    
