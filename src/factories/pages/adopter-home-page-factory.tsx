84885be4bbb6e7ae3f9cc576001b20abbba2d0d7
joelcoelho29 <joelfeco4@gmail.com>
2023-11-26 15:51:57 -0300

    fix: fixed di in adopter home

--- src/factories/pages/adopter-home-page-factory.tsx
+++ src/factories/pages/adopter-home-page-factory.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AdopterHome from "../../presentation/pages/AdopterHome";
+import {makePetInteractor} from "../interactors/pet-interactor-factory";
 
 export const makeAdopterHomePage = () => {
-    return <AdopterHome />
+    return <AdopterHome interactor={makePetInteractor()} />
 }
